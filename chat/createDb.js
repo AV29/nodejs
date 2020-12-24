21fbd8ccbbad6cc2bf2e966ec100593dad1deafb
Anton Vlasik <snumber29@gmail.com>
2020-12-24 23:08:10 +0300

    add mongoose

--- chat/createDb.js
+++ chat/createDb.js
@@ -1,27 +1,13 @@
-const MongoClient = require('mongodb').MongoClient;
-const format = require('util').format;
 
-// Connection URL
-const url = 'mongodb://localhost:27017/chat';
+const schema = mongoose.Schema({
+    name: String
+});
 
-// Database Name
-const dbName = 'chat';
-
-const insertRecord = function(db, record) {
-    return new Promise((resolve, reject) => {
-        const collection = db.collection(dbName);
-        collection.insertOne(record).then(resolve);
-    });
+schema.methods.meow = function() {
+    console.log(this.get('name'));
 };
 
-// Use connect method to connect to the server
-MongoClient.connect(url)
-    .then((client) => {
-        console.log("Connected successfully to server");
-        const db = client.db(dbName);
-        const record = { anton: new Date() };
-        insertRecord(db, record).then(() => {
-            client.close();
-        });
-    })
-    .catch(err => console.error(err));
+const Cat = mongoose.model('Cat', schema);
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(record =>{ console.log(record.meow()); });
