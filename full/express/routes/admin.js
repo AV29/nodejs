8c64ac3ef1a8138754ec98b9a704fd8593ceafec
av29 <snumber29@gmail.com>
2021-04-01 23:50:21 +0300

    add and serve views

--- full/express/routes/admin.js
+++ full/express/routes/admin.js
@@ -1,15 +1,14 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 router.get("/add-product", (req, res) => {
-  res.send(
-    '<form action="/product" method="post"><input type="text" name="product"><button type="submit">Add</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
-  res.redirect("/add-product");
+  res.redirect("/");
 });
 
 module.exports = router;
