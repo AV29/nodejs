e3c354bf9e545b677babaf2bad9abba833412774
av29 <snumber29@gmail.com>
2021-04-01 23:57:06 +0300

    refactor folders for shop

--- full/native/getHtml.js
+++
@@ -1,6 +0,0 @@
-module.exports = (res, body) => {
-    res.write("<html lang='en'>");
-    res.write("<head><title>Simple NodeJS server</title></head>");
-    res.write(body);
-    res.write("</html>");
-};
