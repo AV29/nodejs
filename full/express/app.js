a1c2c98e4baeb46954aa99ace7ccabb8c6d1503e
av29 <snumber29@gmail.com>
2021-04-01 23:22:33 +0300

    add routes

--- full/express/app.js
+++ full/express/app.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use('/users', (req, res) => {
-    res.send('/users got');
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', (req, res) => {
-    res.send('/ got');
+app.use(adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Not Found!</h1>");
 });
 
 app.listen(3000);
