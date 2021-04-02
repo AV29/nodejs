3103aca763fae6d0b1ff566eb6fbae10d27dd4ec
av29 <snumber29@gmail.com>
2021-04-02 13:34:16 +0300

    add prettier and add product.css

--- shop/app.js
+++ shop/app.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const rootDir = require('./utils/path');
+const rootDir = require("./utils/path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(express.static(path.join(rootDir, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(rootDir, "views", "not-found.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "not-found.html"));
 });
 
 app.listen(3000);
