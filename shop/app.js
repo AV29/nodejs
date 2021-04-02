f3157c6a4a87805b76f3c5502be8202e0f138728
av29 <snumber29@gmail.com>
2021-04-02 07:50:23 +0300

    refactor paths

--- shop/app.js
+++ shop/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
-
+const rootDir = require('./utils/path');
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -8,10 +9,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Not Found!</h1>");
+  res
+    .status(404)
+    .sendFile(path.join(rootDir, "views", "not-found.html"));
 });
 
 app.listen(3000);
