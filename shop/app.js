1b455a4aec0c3c5a2daa3db914b2cf5302666a4d
av29 <snumber29@gmail.com>
2021-04-05 12:50:16 +0300

    add handlebars

--- shop/app.js
+++ shop/app.js
@@ -4,17 +4,22 @@ const bodyParser = require("body-parser");
 const rootDir = require("./utils/path");
 const adminRoutes = require("./routes/admin").routes;
 const shopRoutes = require("./routes/shop");
+const initHbs = require("express-handlebars");
 
 const app = express();
-app.set('view engine', 'pug');
-app.set('views', 'views');
+
+app.engine('.hbs', initHbs({
+  extname: '.hbs',
+  defaultLayout: null
+}));
+app.set("view engine", "hbs");
 app.use(express.static(path.join(rootDir, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "not-found.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- shop/routes/shop.js
+++ shop/routes/shop.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const path = require("path");
-const rootDir = require("../utils/path");
 const products = require("./admin").products;
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log(products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop", { pageTitle: "Shop", products: products });
 });
 
 module.exports = router;
