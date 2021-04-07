cffbfbcf315e102701981e798f7fd0195f791d49
av29 <snumber29@gmail.com>
2021-04-07 13:06:00 +0300

    add error controller

--- shop/app.js
+++ shop/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const rootDir = require("./utils/path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 const app = express();
 
@@ -13,8 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
