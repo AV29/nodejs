853248fc7ed1c638d4620c64dd7ebf5ca5219b51
av29 <snumber29@gmail.com>
2021-04-27 21:50:24 +0300

    add user

--- shop/app.js
+++ shop/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongodb = require('mongodb');
 const path = require('path');
 const bodyParser = require('body-parser');
 const rootDir = require('./utils/path');
@@ -6,15 +7,34 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const { mongoConnect } = require('./utils/database');
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(async (req, res, next) => {
+    try {
+        req.user = await User.findById("000000015a6aea6fc450f4f0");
+    } catch (err) {
+        console.error(err);
+    }
+    next();
+});
 app.use(express.static(path.join(rootDir, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-mongoConnect().then(() => {
-    app.listen(3000);
-});
+mongoConnect()
+    .then(() => User.findById("000000015a6aea6fc450f4f0"))
+    .then(user => {
+        if (user) {
+            return Promise.resolve(user);
+        } else {
+            const newUser = new User('Anton', 'snumber29@gmail.com', 1);
+            return newUser.save();
+        }
+    })
+    .then(() => {
+        app.listen(3000);
+    });
--- shop/models/user.js
+++ shop/models/user.js
@@ -1,15 +1,33 @@
-const { Sequelize } = require('sequelize');
-const sequelize = require('../utils/database');
+const mongodb = require('mongodb');
+const { getDb } = require('../utils/database');
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-});
+class User {
+    constructor(name, email, id) {
+        this._id = new mongodb.ObjectId(id);
+        this.name = name;
+        this.email = email;
+    }
+
+    async save() {
+        try {
+            const db = getDb();
+            return await db.collection('users').insertOne(this);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    static async findById(userId) {
+        try {
+            const db = getDb();
+            return await db
+                .collection('users')
+                .find({ _id: new mongodb.ObjectId(userId) })
+                .next();
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
 
 module.exports = User;
