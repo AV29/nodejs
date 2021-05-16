bc71993651860697a006144093baf97ef02d45cc
av29 <snumber29@gmail.com>
2021-05-16 22:41:59 +0300

    add sanitazation

--- shop/utils/validators.js
+++ shop/utils/validators.js
@@ -9,11 +9,15 @@ export const signupValidators = [
             const existingUser = await User.findOne({ email: value });
             if (existingUser) throw new SignupError('This email is already taken!');
             else return true;
-        }),
-    body('password', 'Password length must be 5 characters min').isLength({ min: 5 }),
-    body('confirmPassword', 'Passwords have to match!').custom((value, { req }) => value === req.body.password)
+        })
+        .normalizeEmail(),
+    body('password', 'Password length must be 5 characters min').isLength({ min: 5 }).trim(),
+    body('confirmPassword', 'Passwords have to match!')
+        .custom((value, { req }) => value === req.body.password)
+        .trim()
 ];
 
 export const loginValidators = [
-    body('email', 'Provided email is invalid').isEmail().withMessage('Please enter valid email')
+    body('email', 'Provided email is invalid').isEmail().withMessage('Please enter valid email').normalizeEmail(),
+    body('password', 'Password has to be valid').isLength({ min: 5 }).trim()
 ];
