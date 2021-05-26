45e948b4702d6be32f1a4fe873333f92ba160472
av29 <snumber29@gmail.com>
2021-05-26 15:32:44 +0300

    remove item from the dom when deleted

--- shop/public/js/admin.js
+++ shop/public/js/admin.js
@@ -1,15 +1,15 @@
 const deleteProduct = async btn => {
     const productId = btn.parentNode.querySelector('[name=productId]').value;
     const csrfToken = btn.parentNode.querySelector('[name=_csrf]').value;
-
+    const product = btn.closest('article.card.product-item');
     try {
-        const result = await fetch(`/admin/product/${productId}`, {
+        await fetch(`/admin/product/${productId}`, {
             method: 'DELETE',
             headers: {
                 'csrf-token': csrfToken
             }
         });
-        console.log(result);
+        product.remove();
     } catch (err) {
         console.log(err);
     }
