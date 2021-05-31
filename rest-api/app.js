026039ef68dc6ba4ec79a1479fc768e26cd6a4dd
av29 <snumber29@gmail.com>
2021-06-01 00:12:35 +0300

    add CORS headers for gets

--- rest-api/app.js
+++ rest-api/app.js
@@ -4,6 +4,12 @@ import feedRoutes from './routes/feed.js';
 
 const app = express();
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use('/feed', feedRoutes);
 
 try {
