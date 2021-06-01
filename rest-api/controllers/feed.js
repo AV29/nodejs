a1470c8ef6fa7698773118b977f6bd922db34883
av29 <snumber29@gmail.com>
2021-06-01 23:16:28 +0300

    save a post

--- rest-api/controllers/feed.js
+++ rest-api/controllers/feed.js
@@ -1,5 +1,5 @@
 import { validationResult } from 'express-validator';
-
+import Post from '../models/post.js';
 export const getPosts = async (req, res, next) => {
     res.status(200).json({
         posts: [
@@ -18,23 +18,29 @@ export const getPosts = async (req, res, next) => {
 };
 
 export const createPost = async (req, res, next) => {
-    const errors = validationResult(req);
+    try {
+        const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ message: 'Validation failed!', errors: errors.array() });
-    }
-    const title = req.body.title;
-    const content = req.body.content;
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: {
-            id: new Date().toISOString(),
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ message: 'Validation failed!', errors: errors.array() });
+        }
+        const title = req.body.title;
+        const content = req.body.content;
+        const post = new Post({
             title: title,
             content: content,
-            createdAt: new Date(),
+            imageUrl: 'images/Venice.jpg',
             creator: {
                 name: 'Anton'
             }
-        }
-    });
+        });
+        const result = await post.save();
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post: result
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: 'Something happened!'});
+    }
 };
