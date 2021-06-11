b8ecf6a05c077d4e562263d526332a6f43fd30b9
av29 <snumber29@gmail.com>
2021-06-12 00:54:04 +0300

    clean images up

--- front-end/src/pages/Feed/Feed.js
+++ front-end/src/pages/Feed/Feed.js
@@ -43,7 +43,9 @@ class Feed extends Component {
     this.loadPosts();
     const socket = openSocket("http://localhost:8080");
     socket.on('posts', data => {
-      console.log('HERE', data);
+      if(data.action === 'create') {
+        this.addPost(data.post);
+      }
     })
   }
 
@@ -186,8 +188,6 @@ class Feed extends Component {
               (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
           }
           return {
             posts: updatedPosts,
