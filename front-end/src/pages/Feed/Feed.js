26d315d9ceef9de500cc6bb5e0e42a2cced4e149
av29 <snumber29@gmail.com>
2021-06-12 00:59:13 +0300

    fix adding posts

--- front-end/src/pages/Feed/Feed.js
+++ front-end/src/pages/Feed/Feed.js
@@ -49,18 +49,18 @@ class Feed extends Component {
     })
   }
 
-  addPost = (post) => {
-    this.setState((prevState) => {
+  addPost = post => {
+    this.setState(prevState => {
       const updatedPosts = [...prevState.posts];
       if (prevState.postPage === 1) {
         if (prevState.posts.length >= 2) {
           updatedPosts.pop();
-          updatedPosts.unshift(post);
         }
+        updatedPosts.unshift(post);
       }
       return {
         posts: updatedPosts,
-        totalPosts: prevState.totalPosts + 1,
+        totalPosts: prevState.totalPosts + 1
       };
     });
   };
