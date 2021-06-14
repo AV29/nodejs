7ac7b45675c1957f9952d04e7a4015a1db7d6162
av29 <snumber29@gmail.com>
2021-06-14 23:24:57 +0300

    cleanup

--- rest-api/app.js
+++ rest-api/app.js
@@ -3,10 +3,6 @@ import { fileURLToPath } from 'node:url';
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import socket from './socket.js';
-import feedRoutes from './routes/feed.js';
-import authRoutes from './routes/auth.js';
-import userRoutes from './routes/user.js';
 import MONGODB_URI from './utils/constants.js';
 import cors from './middlewares/cors.js';
 import handleAllErrors from './middlewares/error.js';
@@ -18,9 +14,6 @@ app.use(bodyParser.json());
 app.use(imageUpload);
 app.use('/images', express.static(path.join(rootPath, 'images')));
 app.use(cors);
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
 app.use(handleAllErrors);
 
 try {
@@ -29,11 +22,7 @@ try {
         useUnifiedTopology: true,
         useCreateIndex: true
     });
-    const server = app.listen(8080);
-    const io = socket.init(server);
-    io.on('connection', socket => {
-        console.log('Client connected!!');
-    });
+    app.listen(8080);
 } catch (err) {
     console.log(err);
 }
