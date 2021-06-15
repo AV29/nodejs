50df154293dab6a5e9ccb5b7c755abcd3d265a42
av29 <snumber29@gmail.com>
2021-06-15 08:58:24 +0300

    add signup resolver

--- graph-ql/graphql/resolvers.js
+++ graph-ql/graphql/resolvers.js
@@ -1,2 +1,30 @@
+import User from '../models/user.js';
+import { HttpError } from '../utils/errors.js';
+import bcrypt from 'bcryptjs';
+
 export default {
+    createUser: async function (args, req) {
+        const {
+            userInput: { email, name, password }
+        } = args;
+
+        const existingUser = await User.findOne({ email: email });
+        if (existingUser) {
+            throw new HttpError(401, 'User Already exists');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = new User({
+            email: email,
+            name: name,
+            password: hashedPassword
+        });
+
+        const createdUser = await user.save();
+
+        return {
+            ...createdUser.toJSON(),
+            _id: createdUser._id.toString()
+        };
+    }
 };
