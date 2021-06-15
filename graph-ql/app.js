30de2e1487d384d1623c997fa5b8da2e8a04d968
av29 <snumber29@gmail.com>
2021-06-15 09:16:17 +0300

    add some error handling

--- graph-ql/app.js
+++ graph-ql/app.js
@@ -22,7 +22,15 @@ app.use(
     graphqlHTTP({
         schema: graphqlSchema,
         rootValue: graphqlResolver,
-        graphiql: true
+        graphiql: true,
+        customFormatErrorFn: function (err) {
+            if (!err.originalError) {
+                return err;
+            } else {
+                const { message = 'An error occurred!', status = 500 } = err.originalError;
+                return { message: message, status: status };
+            }
+        }
     })
 );
 app.use(handleAllErrors);
