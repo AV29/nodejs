ec6188aa0cd7d1c7eb5bfda8861717bda88fd804
av29 <snumber29@gmail.com>
2021-06-16 23:23:05 +0300

    connect FE to graphql

--- graph-ql/middlewares/cors.js
+++ graph-ql/middlewares/cors.js
@@ -3,5 +3,8 @@ export default (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 };
