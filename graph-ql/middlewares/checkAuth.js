33a761aa06e5562de02e3e407e46235ca53e9cb3
av29 <snumber29@gmail.com>
2021-06-23 18:57:55 +0300

    update middleware to check auth

--- graph-ql/middlewares/checkAuth.js
+++ graph-ql/middlewares/checkAuth.js
@@ -1,21 +1,24 @@
 import jwt from 'jsonwebtoken';
-import { HttpError } from '../utils/errors.js';
 
 export default (req, res, next) => {
     const authHeader = req.get('Authorization');
     if (!authHeader) {
-        return next(new HttpError(401, 'Not authenticated'));
+        req.isAuth = false;
+        return next();
     }
     let decodedToken;
     try {
         const token = authHeader.split(' ')[1];
         decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     } catch (err) {
-        return next(new HttpError(500, 'Token decoding failed'));
+        req.isAuth = false;
+        return next();
     }
     if (!decodedToken) {
-        return next(new HttpError(401, 'Not authenticated'));
+        req.isAuth = false;
+        return next();
     }
+    req.isAuth = true;
     req.userId = decodedToken.userId;
     next();
 };
