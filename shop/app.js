fa3e041746207930431e8465ce7cd12855f54726
av29 <snumber29@gmail.com>
2021-07-12 21:42:59 +0200

    remove redundant code. Prepare to deploy on heroku

--- shop/app.js
+++ shop/app.js
@@ -52,7 +52,6 @@ try {
         useCreateIndex: true
     });
     https.createServer({ key: privateKey, cert: certificate }, app).listen(process.env.PORT || 3000);
-    //app.listen(3000);
 } catch (err) {
     console.log(err);
 }
