03a118f7c48a767b0dd5066e5732a8dd6fc50ded
Anton Vlasik <antonvlasik@coherentsolutions.com>
2021-08-15 23:31:16 +0200

    test commit

--- shop/test/start.js
+++ shop/test/start.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 
 it('should add two numbers correctly', () => {
-    const num1 = 2;
+    const num1 = 3;
     const num2 = 3;
 
-    expect(num1 + num2).to.equal(5);
+    expect(num1 + num2).to.equal(6);
 });
